3cd5184bcf84df5f586bedbebafe1062596c26d5
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-17 22:08:32 +0500

    3.	Find the Amount of Potatoes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 // //Task1
 
-let num = +prompt ("Enter number")
-alert(Math.pow(0, num))
+// let num = +prompt ("Enter number")
+// alert(Math.pow(0, num))
+
+// //Task 2
+
+// let str = prompt ("Enter your words")
+// cnt="";
+// for (let i=0; i<=str.length; i++){
+//    cnt=cnt+str.charAt(i)+str.charAt(i)
+// }
+// alert(cnt)
+
+
+// //Task 3
+
+let str = prompt ("Enter your words")
+cnt=0;
+potato=""
+
+let find = str.match(/potato/g)
+alert(find.length)
+
+
+
+// Task 4
+
+// let numbers = prompt ("Enter something")
+// alert(Number.numbers)
